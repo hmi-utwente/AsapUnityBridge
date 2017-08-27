44d39a99245502884a622ad7553c623c019f47b1
Jan Kolkmeier <jankolkmeier@gmail.com>
2017-08-27 21:48:42 +0200

    Asap Starter

---
+++ AsapStarter/src/nl/utwente/hmi/starters/UnityAsapStarter.java
@@ -0,0 +1,104 @@
+package nl.utwente.hmi.starters;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import asap.bml.ext.bmlt.BMLTInfo;
+import asap.environment.AsapEnvironment;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
+import hmi.audioenvironment.AudioEnvironment;
+import hmi.environmentbase.ClockDrivenCopyEnvironment;
+import hmi.environmentbase.Environment;
+import hmi.mixedanimationenvironment.MixedAnimationEnvironment;
+import hmi.physicsenvironment.OdePhysicsEnvironment;
+import hmi.worldobjectenvironment.WorldObjectEnvironment;
+import saiba.bml.BMLInfo;
+import saiba.bml.core.FaceLexemeBehaviour;
+import saiba.bml.core.HeadBehaviour;
+import saiba.bml.core.PostureShiftBehaviour;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UnityAsapStarter {
+
+	final static Logger logger = LoggerFactory.getLogger(UnityAsapStarter.class.getName());
+	private String spec;
+	
+    public static void main(String[] args) throws IOException {
+		LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+		JoranConfigurator configurator = new JoranConfigurator();
+	    configurator.setContext(context);
+	    ClassLoader loader = UnityAsapStarter.class.getClassLoader();
+	    context.reset(); 
+	    try {
+			configurator.doConfigure(loader.getResource("UnityAsapLogs.xml"));
+		} catch (JoranException e) {
+			e.printStackTrace();
+		}
+	    StatusPrinter.printInCaseOfErrorsOrWarnings(context);
+	    
+		String help = "Expecting commandline arguments in the form of \"-<argname> <arg>\".\nAccepting the following argnames: agentspec";
+        String spec = "Unity/agentspecs/uma_f.xml";
+        if(args.length % 2 != 0) {
+        	logger.info(help);
+        	System.exit(0);
+        }
+    	
+        for(int i = 0; i < args.length; i = i + 2){
+        	if(args[i].equals("-agentspec")) {
+        		spec = args[i+1];
+        	} else {
+        		logger.warn("Unknown commandline argument: \""+args[i]+" "+args[i+1]+"\".\n"+help);
+            	System.exit(0);
+        	}
+        }
+        
+    	UnityAsapStarter uas = new UnityAsapStarter(spec);
+    	uas.init();
+    }
+
+    public UnityAsapStarter(String spec) {
+    	this.spec = spec;
+    }
+
+    public void init() throws IOException {
+        MixedAnimationEnvironment mae = new MixedAnimationEnvironment();
+        final OdePhysicsEnvironment ope = new OdePhysicsEnvironment();
+        WorldObjectEnvironment we = new WorldObjectEnvironment();
+        AudioEnvironment aue = new AudioEnvironment("LJWGL_JOAL");
+
+        BMLTInfo.init();
+        BMLInfo.addCustomFloatAttribute(FaceLexemeBehaviour.class, "http://asap-project.org/convanim", "repetition");
+        BMLInfo.addCustomStringAttribute(HeadBehaviour.class, "http://asap-project.org/convanim", "spindirection");
+        BMLInfo.addCustomFloatAttribute(PostureShiftBehaviour.class, "http://asap-project.org/convanim", "amount");
+
+        ArrayList<Environment> environments = new ArrayList<Environment>();
+        final AsapEnvironment ee = new AsapEnvironment();
+        
+        ClockDrivenCopyEnvironment ce = new ClockDrivenCopyEnvironment(1000 / 20);
+
+        ce.init();
+        ope.init();
+        mae.init(ope, 0.002f);
+        we.init();
+        aue.init();
+        environments.add(ee);
+        environments.add(ope);
+        environments.add(mae);
+        environments.add(we);
+
+        environments.add(ce);
+        environments.add(aue);
+
+        ee.init(environments, ope.getPhysicsClock());
+        ope.addPrePhysicsCopyListener(ee);
+
+        ee.loadVirtualHuman("", spec, "AsapRealizer demo");
+
+        ope.startPhysicsClock();
+    }
+}
+
--- README.md
+++ README.md
@@ -22,4 +22,35 @@ Running this scene doesn't do much. The agent animation is driven by ASAP.
 
 ### Configuring ASAP
 
-TODO
+Download a copy of hmibuild [from the ASAP repository](https://github.com/ArticulatedSocialAgentsPlatform/hmibuild) and place it in the same folder that AsapUnityBridge is in. Name it hmibuild (not hmibuild-master).
+
+Enter the AsapUnityBridge/AsapStarter folder and run the following programs:
+
+```ant resolve```  
+
+to download the dependencies, and:
+
+```ant main```  
+
+to select the main file to run (the default is fine, just press enter.
+
+```ant run```  
+
+starts the ASAP backend that drives the character. A little window will pop up where you can type in and execute BML.
+
+**NOTE:** These programs require you to have [Apache ANT](http://ant.apache.org/) installed and in your PATH, as well as [Python 2.7](https://www.python.org/downloads/release/python-2713/).
+
+
+### Putting it together
+Start the Unity scene and the ASAP backend (any order is fine). ASAP will initialize itself for the character in the Unity scene (that may take a moment), then BML can be executed.
+
+## TODO's for this documentation
+
+ - Explain the ASAP infrastructure, and where to find what ([here is a start](http://asap-project.ewi.utwente.nl/))
+ - Provide example BMLs.
+ - Point to resources for the procedurally generated characters (UMA, [here is a start](http://umawiki.secretanorak.com/Main_Page))
+ - Describe how to use your own characters, retargeting, etc.
+ - How to use the animation editor and exporting procedural animation files for ASAP gesture binding.
+ - Multi character setups.
+
+
